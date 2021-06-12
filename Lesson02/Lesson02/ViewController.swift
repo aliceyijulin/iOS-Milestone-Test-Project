ac970e67534fd439ed9477860297a7e35bf261e2
aliceyijulin <aliceyijulin@gmail.com>
2021-06-11 20:26:40 -0700

    added Lesson02

---
+++ Lesson02/Lesson02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson02
+//
+//  Created by Alice on 6/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
