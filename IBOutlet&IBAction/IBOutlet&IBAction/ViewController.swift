1fa71e14a4d9f8ecc1fb3014c8f6a69dd3e5fbed
aliceyijulin <aliceyijulin@gmail.com>
2021-06-13 15:44:20 -0700

    fixed updates

--- IBOutlet&IBAction/IBOutlet&IBAction/ViewController.swift
+++ IBOutlet&IBAction/IBOutlet&IBAction/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayBox: UILabel!
+    
+    let quote1 = "Bonjour"
+    let quote2 = "Hola"
+    let quote3 = "你好"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonController(_ sender: Any) {
 
+        let n = Int.random(in: 1...3)
+        
+        if n == 1
+        {
+            displayBox.text = quote1;
+
+        }
+        if n == 2
+        {
+            displayBox.text = quote2;
+        }
+        if n == 3
+        {
+            displayBox.text = quote3;
+        }
+        
+    }
+    
 }
 
