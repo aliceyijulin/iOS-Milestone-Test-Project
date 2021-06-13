3b28d9b998d8b1b5b53b2b8a79ce975b81b22a29
aliceyijulin <aliceyijulin@gmail.com>
2021-06-12 18:47:51 -0700

    Lesson 3 updates

---
+++ StackViewExercise/StackViewExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewExercise
+//
+//  Created by Alice on 6/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
